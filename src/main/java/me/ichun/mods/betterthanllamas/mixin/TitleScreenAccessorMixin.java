141435292d9d8c7bc02ee8c477d82ef4a0870257
iChun <ohai.iChun@gmail.com>
2023-01-24 23:07:48 +0000

    General cleanup as more dev happens

--- src/main/java/me/ichun/mods/betterthanllamas/mixin/TitleScreenAccessorMixin.java
+++ src/main/java/me/ichun/mods/betterthanllamas/mixin/TitleScreenAccessorMixin.java
@@ -7,6 +7,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(TitleScreen.class)
 public interface TitleScreenAccessorMixin
 {
-    @Accessor("splash")
+    @Accessor
     void setSplash(String newSplash);
 }
