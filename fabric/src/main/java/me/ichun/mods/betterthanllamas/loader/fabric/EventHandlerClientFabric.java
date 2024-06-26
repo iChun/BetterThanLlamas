091cf9185f004a0313ed48c25ddadb4d11627e22
iChun <ohai.iChun@gmail.com>
2024-06-26 15:52:39 +0100

    Backport 1.19.3 to 1.19.2. Use of iChunUtil's Gradle workspace for this, lots of files moved around/removed.

---
+++ fabric/src/main/java/me/ichun/mods/betterthanllamas/loader/fabric/EventHandlerClientFabric.java
@@ -0,0 +1,21 @@
+package me.ichun.mods.betterthanllamas.loader.fabric;
+
+import me.ichun.mods.betterthanllamas.common.BetterThanLlamas;
+import me.ichun.mods.betterthanllamas.common.core.Config;
+import me.ichun.mods.betterthanllamas.common.core.EventHandlerClient;
+import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
+import net.minecraft.client.renderer.entity.LlamaRenderer;
+import net.minecraft.world.entity.EntityType;
+
+public class EventHandlerClientFabric extends EventHandlerClient
+{
+    public EventHandlerClientFabric()
+    {
+        LivingEntityFeatureRendererRegistrationCallback.EVENT.register((entityType, entityRenderer, registrationHelper, context) -> {
+            if((BetterThanLlamas.config.applyOn == Config.ApplyOn.ALL || (entityType == EntityType.LLAMA && BetterThanLlamas.config.applyOn == Config.ApplyOn.LLAMA) || (entityType == EntityType.TRADER_LLAMA && BetterThanLlamas.config.applyOn == Config.ApplyOn.TRADER_LLAMA)) && entityRenderer instanceof LlamaRenderer llamaRenderer)
+            {
+                addFancyLayer(llamaRenderer);
+            }
+        });
+    }
+}
