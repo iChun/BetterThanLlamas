42953b8f48eef090ab7141f26b424627ced042e9
iChun <ohai.iChun@gmail.com>
2024-06-26 03:19:53 +0100

    Backport 1.20.6 to 1.20.4.

--- neoforge/src/main/java/me/ichun/mods/betterthanllamas/loader/neoforge/LoaderNeoForge.java
+++ neoforge/src/main/java/me/ichun/mods/betterthanllamas/loader/neoforge/LoaderNeoForge.java
@@ -10,7 +10,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 
 @Mod(BetterThanLlamas.MOD_ID)
@@ -38,6 +38,6 @@ public class LoaderNeoForge extends BetterThanLlamas
 
         new EventHandlerClientNeoForge(modEventBus);
 
-        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (minecraft, screen) -> new WorkspaceConfigs(screen));
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new WorkspaceConfigs(screen)));
     }
 }
