c46f32c4f64a2d0f20d3068348373730aac6f225
iChun <ohai.iChun@gmail.com>
2024-06-26 18:40:15 +0100

    Backport 1.19.2 to 1.18.2. Use of iChunUtil's Gradle workspace for this, lots of files moved around/removed. RIP some of iChunUtil's ported code as this commit removes them as they weren't ported to 1.20.6.

---
+++ common/src/main/java/me/ichun/mods/betterthanllamas/common/core/EventHandlerClient.java
@@ -0,0 +1,38 @@
+package me.ichun.mods.betterthanllamas.common.core;
+
+import me.ichun.mods.betterthanllamas.client.render.LlamaFancyLayer;
+import me.ichun.mods.betterthanllamas.mixin.LlamaDecorLayerAccessorMixin;
+import me.ichun.mods.ichunutil.mixin.client.LivingEntityRendererAccessorMixin;
+import net.minecraft.client.model.LlamaModel;
+import net.minecraft.client.renderer.entity.LlamaRenderer;
+import net.minecraft.client.renderer.entity.layers.LlamaDecorLayer;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.world.entity.animal.horse.Llama;
+
+public abstract class EventHandlerClient
+{
+    @SuppressWarnings("unchecked")
+    public void addFancyLayer(LlamaRenderer llamaRenderer)
+    {
+        boolean flag = false;
+        for(RenderLayer<Llama, LlamaModel<Llama>> layer : ((LivingEntityRendererAccessorMixin<Llama, LlamaModel<Llama>>)llamaRenderer).getLayers())
+        {
+            if(layer instanceof LlamaFancyLayer)
+            {
+                flag = true;
+                break;
+            }
+
+        }
+        if(!flag)
+        {
+            LlamaFancyLayer fancyLayer = new LlamaFancyLayer(llamaRenderer);
+            ((LivingEntityRendererAccessorMixin<Llama, LlamaModel<Llama>>)llamaRenderer).invokeAddLayer(fancyLayer);
+            if(fancyLayer.isEasterEggDay)
+            {
+                ((LivingEntityRendererAccessorMixin<Llama, LlamaModel<Llama>>)llamaRenderer).getLayers().stream().filter(l -> l instanceof LlamaDecorLayer).forEach(layer -> LlamaFancyLayer.processLlamaModelForEE(((LlamaDecorLayerAccessorMixin)(LlamaDecorLayer)layer).getModel()));
+            }
+        }
+
+    }
+}
