e1de112eca7868a0e879af80f3c3b09597e2903d
iChun <ohai.iChun@gmail.com>
2024-06-26 06:43:07 +0100

    Backport 1.20.2 to 1.20.1. Use of iChunUtil's Gradle workspace for this, lots of files moved around/removed.

---
+++ forge/src/main/java/me/ichun/mods/betterthanllamas/loader/forge/EventHandlerClientForge.java
@@ -0,0 +1,40 @@
+package me.ichun.mods.betterthanllamas.loader.forge;
+
+import me.ichun.mods.betterthanllamas.common.BetterThanLlamas;
+import me.ichun.mods.betterthanllamas.common.core.Config;
+import me.ichun.mods.betterthanllamas.common.core.EventHandlerClient;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.LlamaRenderer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.horse.Llama;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+public class EventHandlerClientForge extends EventHandlerClient
+{
+    public EventHandlerClientForge()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onAddLayers);
+    }
+
+    private void onAddLayers(EntityRenderersEvent.AddLayers event)
+    {
+        if(BetterThanLlamas.config.applyOn == Config.ApplyOn.LLAMA || BetterThanLlamas.config.applyOn == Config.ApplyOn.ALL)
+        {
+            LivingEntityRenderer<Llama, ? extends EntityModel<Llama>> render = event.getRenderer(EntityType.LLAMA);
+            if(render instanceof LlamaRenderer llamaRenderer)
+            {
+                addFancyLayer(llamaRenderer);
+            }
+        }
+        if(BetterThanLlamas.config.applyOn == Config.ApplyOn.TRADER_LLAMA || BetterThanLlamas.config.applyOn == Config.ApplyOn.ALL)
+        {
+            LivingEntityRenderer<Llama, ? extends EntityModel<Llama>> render = event.getRenderer(EntityType.TRADER_LLAMA);
+            if(render instanceof LlamaRenderer llamaRenderer)
+            {
+                addFancyLayer(llamaRenderer);
+            }
+        }
+    }
+}
