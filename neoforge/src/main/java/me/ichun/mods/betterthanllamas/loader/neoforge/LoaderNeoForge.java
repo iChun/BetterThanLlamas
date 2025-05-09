a2d36924d99fde1713c4e10599592309a7afeaf8
iChun <ohai.iChun@gmail.com>
2025-05-09 19:27:25 +0100

    Update Neoforge config registration to pass in ModContainer.

--- neoforge/src/main/java/me/ichun/mods/betterthanllamas/loader/neoforge/LoaderNeoForge.java
+++ neoforge/src/main/java/me/ichun/mods/betterthanllamas/loader/neoforge/LoaderNeoForge.java
@@ -36,7 +36,7 @@ public class LoaderNeoForge extends BetterThanLlamas
     private void initClient(IEventBus modEventBus, ModContainer container)
     {
         //register config
-        config = iChunUtil.d().registerConfig(new Config(), modEventBus);
+        config = iChunUtil.d().registerConfig(new Config(), modEventBus, container);
 
         eventHandlerClient = new EventHandlerClientNeoForge(modEventBus);
 
